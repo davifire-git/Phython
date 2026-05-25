4d5cfed039739a12ddcf4a11495126d8aa813cf4
Davi <davi.brabo133@gmail.com>
2026-05-25 14:21:29 -0300

    .

---
+++ exerciciosguanabara/ex1.py
@@ -0,0 +1,5 @@
+val = int(input())
+
+calc = val ** 0.5
+
+print(f"{val * 2} , {val * 3} e {calc:.2f}")
---
+++ exerciciosguanabara/ex2.py
@@ -0,0 +1,4 @@
+val = float(input("Digite um valor em metros: "))
+
+print(f"Para centímetros será: {val * 100}cm")
+print(f"Para Milímetros será: {val * 1000}mm")
