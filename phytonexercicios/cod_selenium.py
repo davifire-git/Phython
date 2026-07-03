b4cca87bf65cde3a2b4bcb41eca8b0e0e2ee4f22
Davi <davi.brabo133@gmail.com>
2026-07-03 15:50:41 -0300

    .

--- phytonexercicios/cod_selenium.py
+++ phytonexercicios/cod_selenium.py
@@ -9,13 +9,13 @@ import time
 navegador = webdriver.Chrome()
 
 #Acessar um site
-navegador.get("https://gemini.google.com/app")
+navegador.get("https://use.ai/pt?model=claude&utm_match_type=e&utm_funnel=&partner=WM&id=Z29vZ2xlfGNwY3xBSV9CUl9QVF9DaGF0X0RTS19TRUFfTExNX0NsYXVkZXwyMzc0NjkyNDMwOXxjbGF1ZGUgYWl8ODA0Nzc1NzExODg5fHwxOTYxNzI4NzkxOTh8QUlfQlJfUFRfQ2xhdWRlX0V4YWN0fDgwNDc3NTcxMTg4OXx8fHx8fENqd0tDQWp3dTUzU0JoQWhFaXdBSnpTTE5uU0pMTmtjcUhiOE9sOTdBV3kwUDZxYW9aWmtHRFY1SWFSMlZnZkJDU3gwNE5CcnQ0TFdqQm9DeTV3UUF2RF9Cd0V8fHw&url=https%3A%2F%2Fuse.ai%2Fpt%3Fmodel%3Dclaude&gad_source=1&gad_campaignid=23746924309&gbraid=0AAAAACQlLoYthmW6uIqJC41L_iFxSJRdf")
 
 #Tela cheia
 navegador.maximize_window()
 
 #Achando o texto e escrevendo
-#campo_pergunta = navegador.find_element(By.CLASS_NAME, "placeholder")
+#campo_pergunta = navegador.find_element(By.CLASS_NAME, "col-start-1.col-end-2.row-start-1.row-end-2.w-full.select-text.resize-none.overflow-hidden.border-0.bg-transparent.text-base.leading-6.outline-none.[overflow-wrap:break-word].placeholder:whitespace-nowrap.placeholder:font-normal.text-[#0d0d0d].placeholder-[#3d3d3da9].dark:text-foreground.dark:placeholder-[#9ca3af]")
 #campo_pergunta.send_keys("Bom dia")
 #campo_pergunta.send_keys(Keys.ENTER)
 #Clique no botao
